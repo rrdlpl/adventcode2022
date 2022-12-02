39655a53372f0e61b0d71cbcbab2927780a4887c
Roberto De la Parra <roberto.delaparra@ottonova.de>
2022-12-02 12:04:26 +0100

    max k heap

--- day1/day_1_1.py
+++ day1/day_1_1.py
@@ -1,20 +1,17 @@
 
-import heapq
 
 elves = open('input.txt', 'r')
 elvesLines = elves.readlines()
 
 
-maximum = -1 
+maximum = -1
 sum = 0
-weights = []
-heap = heapq.heapify(weights)
 
 for elf in elvesLines:
-  if elf.strip() == '':
-    maximum = max(sum, maximum)
-    sum = 0 
-    continue
-  sum += int(elf.strip())
-  
+    if elf.strip() == '':
+        maximum = max(sum, maximum)
+        sum = 0
+        continue
+    sum += int(elf.strip())
+
 print(maximum)
