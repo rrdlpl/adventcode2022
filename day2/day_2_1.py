cb628febcfac6452eb6654e0363c0939498f9317
Roberto De la Parra <roberto.delaparra@ottonova.de>
2022-12-02 11:38:37 +0100

    removed else

--- day2/day_2_1.py
+++ day2/day_2_1.py
@@ -28,18 +28,16 @@ w = 0
 l = 0
 t = 0
 for line in lines:
-    print('line', line)
     game = line.split()
     enemy, me = game
     enemy = enemy.strip()
     me = me.strip()
-    print('me', myMap[me], enemyMap[enemy])
     if myMap[me] == enemyMap[enemy]:
-        score += 3 + points[me]
+        score += 3
     elif (myMap[me] == 'paper' and enemyMap[enemy] == 'rock') or (myMap[me] == 'rock' and enemyMap[enemy] == 'scissors') or (myMap[me] == 'scissors' and enemyMap[enemy] == 'paper'):
-        score += 6 + points[me]
-    else:
-        score += points[me]
+        score += 6
+
+    score += points[me]
 
 print(w, l, t)
 print(score)
