a8a62e14a5a55d8b568a700793d418f10f4d7a1e
Roberto De la Parra <roberto.delaparra@ottonova.de>
2022-12-02 12:04:26 +0100

    max k heap

--- day1/day_1_2.py
+++ day1/day_1_2.py
@@ -1,18 +1,21 @@
 
 import heapq
 
-elves = open('inputs/day_1.txt', 'r')
+elves = open('day1/input.txt', 'r')
 elvesLines = elves.readlines()
 weights = []
 heap = heapq.heapify(weights)
 sum = 0
 
 for elf in elvesLines:
-  if elf.strip() == '':
-    heapq.heappush(weights, -sum)
-    sum = 0 
-    continue
-  sum += int(elf.strip())
+    if elf.strip() == '':
+        if len(weights) == 3:
+            heapq.heappushpop(weights, sum)
+        else:
+            heapq.heappush(weights, sum)
+        sum = 0
+        continue
+    sum += int(elf.strip())
 
 top1 = abs(heapq.heappop(weights))
 top2 = abs(heapq.heappop(weights))
