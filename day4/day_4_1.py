33bec59174326ad93afc2691fdf7250fa7975734
Roberto De la Parra <roberto.delaparra@ottonova.de>
2022-12-04 08:47:29 +0100

    day4 code

---
+++ day4/day_4_1.py
@@ -0,0 +1,24 @@
+
+
+file = open('day4/input.txt', 'r')
+lines = file.readlines()
+
+
+result = 0
+
+for line in lines:
+
+    line = line.strip()
+    left, right = line.split(',')
+    minA, maxB = left.split('-')
+    # print(minA, maxB)
+    a = set(range(int(minA), int(maxB) + 1))
+
+    minA, maxB = right.split('-')
+    # print(minA, maxB)
+    b = set(range(int(minA), int(maxB) + 1))
+
+    if a.issubset(b) or b.issubset(a):
+        result += 1
+
+print(result)
