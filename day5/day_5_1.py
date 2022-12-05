87c6ec9487eb229273311f41d0d78a30ea624983
Roberto De la Parra <roberto.delaparra@ottonova.de>
2022-12-05 08:59:44 +0100

    day 5

---
+++ day5/day_5_1.py
@@ -0,0 +1,39 @@
+
+
+from collections import deque
+import re
+
+
+file = open('day5/input.txt', 'r')
+lines = file.readlines()
+
+
+result = 0
+
+stacks = [deque(['B', 'L', 'D', 'T', 'W', 'C', 'F', 'M']),
+          deque(['N', 'B', 'L']),
+          deque(['J', 'C', 'H', 'T', 'L', 'V']),
+          deque(['S', 'P', 'J', 'W']),
+          deque(['Z', 'S', 'C', 'F', 'T', 'L', 'R']),
+          deque(['W', 'D', 'G', 'B', 'H', 'N', 'Z']),
+          deque(['F', 'M', 'S', 'P', 'V', 'G', 'C', 'N']),
+          deque(['W', 'Q', 'R', 'J', 'F', 'V', 'C', 'Z']),
+          deque(['R', 'P', 'M', 'L', 'H'])]
+
+for line in lines:
+
+    instruction = [int(s) for s in re.findall(r'\b\d+\b', line)]
+    move = int(instruction[0])
+    fr = int(instruction[1]) - 1
+    to = int(instruction[2]) - 1
+
+    for i in range(move):
+        p = stacks[fr].pop()
+        stacks[to].append(p)
+
+result = []
+for stack in stacks:
+
+    result.append(stack.pop())
+
+print(''.join(result))
