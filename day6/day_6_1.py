727bd82b37b2e6238823af010a27bfecc467f5c6
Roberto De la Parra <roberto.delaparra@ottonova.de>
2022-12-06 08:20:18 +0100

    day6

--- day6/day_6_1.py
+++ day6/day_6_1.py
@@ -1,10 +1,18 @@
 
 
+from collections import Counter
+
+
 file = open('day6/input.txt', 'r')
 lines = file.readlines()
 
-
+size = 14  # 4
 for line in lines:
-    print('df')
+    for i in range(1, len(line)):
+        s = Counter(line[i:(i+size)])
+        if len(s) == size:
+            result = i + size
+            break
 
+print(result)
 file.close()
