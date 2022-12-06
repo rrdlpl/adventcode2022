39ce698bf1ac396d646da0645dd67c38a0b370ca
Roberto De la Parra <roberto.delaparra@ottonova.de>
2022-12-06 08:21:32 +0100

    removed loop

--- day6/day_6_1.py
+++ day6/day_6_1.py
@@ -4,15 +4,14 @@ from collections import Counter
 
 
 file = open('day6/input.txt', 'r')
-lines = file.readlines()
-
+line = file.readlines()[0]
 size = 14  # 4
-for line in lines:
-    for i in range(1, len(line)):
-        s = Counter(line[i:(i+size)])
-        if len(s) == size:
-            result = i + size
-            break
+
+for i in range(1, len(line)):
+    s = Counter(line[i:(i+size)])
+    if len(s) == size:
+        result = i + size
+        break
 
 print(result)
 file.close()
