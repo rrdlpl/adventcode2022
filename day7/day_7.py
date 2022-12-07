2b7141d7ade3c6a09dbe1b2d3f856751d3eeadd9
Roberto De la Parra <roberto.delaparra@ottonova.de>
2022-12-07 11:56:08 +0100

    oop solution

--- day7/day_7.py
+++ day7/day_7.py
@@ -33,61 +33,64 @@ class Folder:
         return sum + self.files_size
 
 
-def build_tree(lines: str):
-    root = Folder('/', None)
-    current_folder = root
-    for i in range(1, len(lines)):
-        input = lines[i].strip().split()
-        if input[0] == '$' and input[1] == 'ls':
-            continue
-        if input[0] == '$' and input[1] == 'cd':
-            if input[2] == '..':
-                current_folder = current_folder.parent
+class Solution:
+    def build_tree(self, lines: str):
+        root = Folder('/', None)
+        current_folder = root
+        for i in range(1, len(lines)):
+            input = lines[i].strip().split()
+            if input[0] == '$' and input[1] == 'ls':
+                continue
+            if input[0] == '$' and input[1] == 'cd':
+                if input[2] == '..':
+                    current_folder = current_folder.parent
+                else:
+                    current_folder = current_folder.folders[input[2]]
+            elif input[0] == 'dir':
+                current_folder.add_folder(Folder(input[1], current_folder))
             else:
-                current_folder = current_folder.folders[input[2]]
-        elif input[0] == 'dir':
-            current_folder.add_folder(Folder(input[1], current_folder))
-        else:
-            current_folder.add_file(File(input[1], int(input[0])))
+                current_folder.add_file(File(input[1], int(input[0])))
 
-    return root
+        return root
+
+    def get_sum_directories(self, root: Folder, max_size: int) -> int:
+        queue = deque()
+        sum = 0
+        queue.append(root)
+        while len(queue) > 0:
+            top = queue.pop()
+            if top.get_folder_size() < max_size:
+                sum += top.get_folder_size()
+            for key in top.folders:
+                queue.append(top.folders[key])
+        return sum
+
+    def directory_to_delete(self, root: Folder, hd_size: int, update_size: int) -> Folder:
+        queue = deque()
+        space_required = update_size - (hd_size - root.get_folder_size())
+
+        queue.append(root)
+        minimum = math.inf
+        folder = None
+
+        while len(queue) > 0:
+            top = queue.pop()
+            if top.get_folder_size() >= space_required:
+                if top.get_folder_size() <= minimum:
+                    minimum = top.get_folder_size()
+                    folder = top
+            for key in top.folders:
+                queue.append(top.folders[key])
+        return folder
+
+    def main(self):
+        root = self.build_tree(lines)
+        print('Solution 1, folder size <= 100000 ',
+              self.get_sum_directories(root, 100000))
+        delete = self.directory_to_delete(root, 70000000, 30000000)
+        print('Solution 2', delete.name, delete.get_folder_size())
 
 
-def get_sum_directories(root: Folder, max_size: int) -> int:
-    queue = deque()
-    sum = 0
-    queue.append(root)
-    while len(queue) > 0:
-        top = queue.pop()
-        if top.get_folder_size() < max_size:
-            sum += top.get_folder_size()
-        for key in top.folders:
-            queue.append(top.folders[key])
-    return sum
+Solution().main()
 
-
-def directory_to_delete(root: Folder, hd_size: int, update_size: int) -> Folder:
-    queue = deque()
-    space_required = update_size - (hd_size - root.get_folder_size())
-
-    queue.append(root)
-    minimum = math.inf
-    folder = None
-
-    while len(queue) > 0:
-        top = queue.pop()
-        if top.get_folder_size() >= space_required:
-            if top.get_folder_size() <= minimum:
-                minimum = top.get_folder_size()
-                folder = top
-        for key in top.folders:
-            queue.append(top.folders[key])
-    return folder
-
-
-root = build_tree(lines)
-print('Solution 1, folder size <= 100000 ', get_sum_directories(root, 100000))
-delete = directory_to_delete(root, 70000000, 30000000)
-
-print('Solution 2', delete.name, delete.get_folder_size())
 file.close()
