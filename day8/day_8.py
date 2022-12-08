1c47948804d4941980453489a85e0135542de12a
Roberto De la Parra <roberto.delaparra@ottonova.de>
2022-12-08 15:32:37 +0100

    renamed some variables

--- day8/day_8.py
+++ day8/day_8.py
@@ -1,55 +1,52 @@
 import time
 
-file = open('day8/input.txt', 'r')
-lines = file.readlines()
 
-
-def build_matrix(lines):
-    matrix = []
+def build_forest(lines):
+    forest = []
     for line in lines:
         line = line.strip()
         row = [int(s) for s in list(line)]
-        matrix.append(row)
+        forest.append(row)
 
-    return matrix
+    return forest
 
 
-def count_visible_trees(matrix):
-    n = len(matrix)
+def count_visible_trees(forest):
+    n = len(forest)
     visited = set()
 
     for i in range(1, n - 1):
-        max_visible_height_top = matrix[0][i]
-        max_visible_height_left = matrix[i][0]
-        max_visible_height_bottom = matrix[n - 1][i]
-        max_visible_height_right = matrix[i][n - 1]
+        max_visible_height_top = forest[0][i]
+        max_visible_height_left = forest[i][0]
+        max_visible_height_bottom = forest[n - 1][i]
+        max_visible_height_right = forest[i][n - 1]
 
         for j in range(1, n - 1):
-            if matrix[i][n - j - 1] > max_visible_height_right:
+            if forest[i][n - j - 1] > max_visible_height_right:
                 coord = (i, n - j - 1)
-                max_visible_height_right = matrix[i][n - j - 1]
+                max_visible_height_right = forest[i][n - j - 1]
                 visited.add(coord)
 
-            if matrix[n - 1 - j][i] > max_visible_height_bottom:
+            if forest[n - 1 - j][i] > max_visible_height_bottom:
                 coord = (n - 1 - j, i)
-                max_visible_height_bottom = matrix[n - 1 - j][i]
+                max_visible_height_bottom = forest[n - 1 - j][i]
                 visited.add(coord)
 
-            if matrix[j][i] > max_visible_height_top:
+            if forest[j][i] > max_visible_height_top:
                 coord = (j, i)
-                max_visible_height_top = matrix[j][i]
+                max_visible_height_top = forest[j][i]
                 visited.add(coord)
 
-            if matrix[i][j] > max_visible_height_left:
+            if forest[i][j] > max_visible_height_left:
                 coord = (i, j)
-                max_visible_height_left = matrix[i][j]
+                max_visible_height_left = forest[i][j]
                 visited.add(coord)
 
     return len(visited) + 4 * (n - 1)
 
 
-def scenic_score(matrix):
-    n = len(matrix)
+def scenic_score(forest):
+    n = len(forest)
 
     def can_move(row, col):
         return row >= 0 and col >= 0 and row < n and col < n
@@ -61,7 +58,7 @@ def scenic_score(matrix):
             counter = 0
             next_row = row
             next_col = col
-            height = matrix[next_row][next_col]
+            height = forest[next_row][next_col]
             while True:
                 x, y = direction
                 next_col += x
@@ -70,7 +67,7 @@ def scenic_score(matrix):
                     break
 
                 counter += 1
-                if height <= matrix[next_row][next_col]:
+                if height <= forest[next_row][next_col]:
                     break
             score *= counter
         return score
@@ -84,13 +81,17 @@ def scenic_score(matrix):
     return calc()
 
 
-matrix = build_matrix(lines)
+file = open('day8/input.txt', 'r')
+lines = file.readlines()
+file.close()
 
-print(count_visible_trees(matrix))
+forest = build_forest(lines)
+
+print('First', count_visible_trees(forest))
 start_time = time.time()
-print('highest score', scenic_score(matrix))
+print('Second', scenic_score(forest))
 end_time = time.time()
 time_elapsed = end_time - start_time
-print('time elapsed', time_elapsed * 1000)
+print('Time elapsed', time_elapsed * 1000)
 
-file.close()
+# file.close()
