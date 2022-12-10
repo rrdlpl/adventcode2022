50388bde2de9a9f7226fa0b156e450beffff98ab
Roberto De la Parra <roberto.delaparra@ottonova.de>
2022-12-10 19:23:02 +0100

    day 10

--- day9/day_9.py
+++ day9/day_9.py
@@ -1,10 +1,9 @@
-import matplotlib.pyplot as plt
-from matplotlib import animation
+# import matplotlib.pyplot as plt
+# from matplotlib import animation
 
 import math
 import time
 
-# print(math.dist([0, 2], [2, 2]))
 
 moves = {
     'R': (1, 0),
@@ -75,9 +74,8 @@ def simulate(lines: str):
 
 def simulate2(lines: str):
     rope = [(0, 0) for _ in range(10)]
-    tail = rope[0]
     visited = set()
-    visited.add(tail)
+    visited.add(rope[0])
     paths = []
     paths.append(rope.copy())
 
@@ -89,8 +87,10 @@ def simulate2(lines: str):
         # print('Head at', head)
         for _ in range(int(n)):
             x, y = head
-            x += moves[instruction][0]
-            y += moves[instruction][1]
+            dx = moves[instruction][0]
+            dy = moves[instruction][1]
+            x += dx
+            y += dy
             head = (x, y)
             rope[-1] = (x, y)
             for i in range(len(rope) - 2, -1, -1):
@@ -99,8 +99,11 @@ def simulate2(lines: str):
                 h_x, h_y = h
                 prev_x, prev_y = prev
 
-                if math.dist(h, prev) >= 2.0:
-                    if h_x > prev_x:
+                if h == prev:
+                    break
+                
+                if math.dist(prev, h) >= 2.0:
+                    if h_x > prev_x: 
                         prev_x += 1
                     elif h_x < prev_x:
                         prev_x -= 1
@@ -109,9 +112,12 @@ def simulate2(lines: str):
                     elif h_y < prev_y:
                         prev_y -= 1
                     rope[i] = (prev_x, prev_y)
+                   
 
+            
             print(rope)
-            paths.append(rope.copy())
+            
+            # paths.append(rope.copy())
             visited.add(rope[0])
 
     return (visited, paths)
@@ -135,39 +141,39 @@ file.close()
 # print('paths', paths)
 
 # Create some data to plot
-x = []
-y = []
-for path in paths:
-    for coord in path:
-        x.append(coord[0])
-        y.append(coord[1])
+# x = []
+# y = []
+# for path in paths:
+#     for coord in path:
+#         x.append(coord[0])
+#         y.append(coord[1])
 
 
 
 
-# Create a figure and axis
-fig, ax = plt.subplots()
+# # Create a figure and axis
+# fig, ax = plt.subplots()
 
-# Create a scatter plot
-scatter, = ax.plot(x, y, 'bo')
+# # Create a scatter plot
+# scatter, = ax.plot(x, y, 'bo')
 
-# This function will be called to update the plot
-def update(num):
-    # Update the data for the scatter plot
-    scatter.set_data(x[num-10:num], y[num-10:num])
-    colors = ['crimson', 'darkorange', 'lime', 'cornflowerblue', 'purple', 'fuchsia']
-    scatter.set_color(colors[num % len(colors)])
+# # This function will be called to update the plot
+# def update(num):
+#     # Update the data for the scatter plot
+#     scatter.set_data(x[num-10:num], y[num-10:num])
+#     colors = ['crimson', 'darkorange', 'lime', 'cornflowerblue', 'purple', 'fuchsia']
+#     scatter.set_color(colors[num % len(colors)])
 
     
-    return scatter,
+#     return scatter,
 
-# Create the animation
-plt.axis([min(x) - 10, max(x) + 10, min(y) - 10, max(y) + 10])
+# # Create the animation
+# plt.axis([min(x) - 10, max(x) + 10, min(y) - 10, max(y) + 10])
 
-anim = animation.FuncAnimation(fig, update, frames=len(x), interval=100)
-# FFwriter=animation.FFMpegWriter(fps=10, extra_args=['-vcodec', 'libx264'])
+# anim = animation.FuncAnimation(fig, update, frames=len(x), interval=100)
+# # FFwriter=animation.FFMpegWriter(fps=10, extra_args=['-vcodec', 'libx264'])
 
-anim.save('my_animation2.gif', fps=30)
+# anim.save('my_animation2.gif', fps=30)
 
-# Show the plot
-plt.show()
+# # Show the plot
+# plt.show()
