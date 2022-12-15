17280876f68ebe44e3e227ab23a4abdcfeaf0427
Roberto De la Parra <roberto.delaparra@ottonova.de>
2022-12-15 15:06:10 +0100

    optimized solution

--- day15/day_15p1.py
+++ day15/day_15p1.py
@@ -50,7 +50,7 @@ def parse_input(lines):
     return sensors
 
 
-def solution_one(sensors, target_y):
+def solution_one(sensors, y):
     ranges_without_beacon = {
 
     }
@@ -58,41 +58,56 @@ def solution_one(sensors, target_y):
     sensors_at_y = []
     for sensor in sensors:
         beacon = sensors[sensor]
-        set_ranges(sensor, beacon, ranges_without_beacon, target_y)
-        if sensor.y == target_y:
+        set_ranges(sensor, beacon, ranges_without_beacon)
+        if sensor.y == y:
             sensors_at_y.append(sensor)
 
-    first_left, first_right = ranges_without_beacon[target_y][0]
+    first_left, first_right = ranges_without_beacon[y][0]
     interv = interval(min(first_left.x, first_right.x),
                       max(first_left.x, first_right.x))
-
-    for i in range(1, len(ranges_without_beacon[target_y])):
-        left, right = ranges_without_beacon[target_y][i]
-
+    for i in range(1, len(ranges_without_beacon[y])):
+        left, right = ranges_without_beacon[y][i]
+        print('left', left)
+        print('right', right)
         min_x = min(left.x, right.x)
         max_x = max(left.x, right.x)
-
+        # print(interv | interval[min_x, max_x])
         interv = interv | interval[min_x, max_x]
-
+        # print('Range', range)
     print('Interval', interv)
-    print('Solution 1.', interv)
+    print('sum', len(interv))
 
 
-def set_ranges(sensor, beacon, ranges_without_beacon, target_y):
+def set_ranges(sensor, beacon, ranges_without_beacon):
     radius = (sensor - beacon).get_radius()
+    diameter = (radius * 2) + 1
     x = sensor.x
+    left = Vector2D(x - radius, sensor.y)
+    right = Vector2D(x + radius, sensor.y)
 
-    if (sensor.y < target_y and sensor.y + radius < target_y) or (sensor.y > target_y and sensor.y - radius > target_y):
-        return
+    adjust_overlapping_range(sensor.y, ranges_without_beacon, left, right)
 
-    dy = abs(target_y - sensor.y)
-    print('Radius ', radius)
-    print('Sensor => ', sensor, 'dy', dy)
-    left = Vector2D(x - radius + dy, target_y)
-    right = Vector2D(x + radius - dy, target_y)
-    print('  adding left', left)
-    print('  adding right', right)
-    adjust_overlapping_range(target_y, ranges_without_beacon, left, right)
+    y_up = sensor.y - 1
+    y_down = sensor.y + 1
+    radius -= 1
+
+    while diameter >= 1:
+        left_up = Vector2D(x - radius, y_up)
+        right_up = Vector2D(x + radius, y_up)
+        left_down = Vector2D(x - radius, y_down)
+        right_down = Vector2D(x + radius, y_down)
+
+        adjust_overlapping_range(
+            y_up, ranges_without_beacon, left_up, right_up)
+
+        adjust_overlapping_range(
+            y_down, ranges_without_beacon, left_down, right_down)
+
+        y_up -= 1
+        y_down += 1
+        radius -= 1
+        diameter -= 2
+    return radius
 
 
 def adjust_overlapping_range(y, ranges_without_beacon, left, right):
@@ -103,6 +118,7 @@ def adjust_overlapping_range(y, ranges_without_beacon, left, right):
 
 
 start_time = time.time()
+
 sensors = parse_input(lines)
 solution_one(sensors, 2_000_000)
 
