7a02541c3fbcc6d0e4b8120c994d31f402373c1a
Roberto De la Parra <roberto.delaparra@ottonova.de>
2024-12-02 14:32:45 +0100

    refactor

--- 2024/day2/day2_1.py
+++ 2024/day2/day2_1.py
@@ -6,24 +6,21 @@ result = 0
 a = []
 b = []
 
-def isSafe(a):
-  is_increasing = a[0] < a[1]
-  for i in range(len(a) - 1):
-      # print('diff between ', a[i], 'and', a[i + 1], 'is', (a[i] - a[i + 1]))
-      diff = a[i] - a[i + 1]
-      if is_increasing and diff > 0:
-        return False
-      if not is_increasing and diff < 0:
-        return False
-      if abs(diff) < 1 or abs(diff) > 3:
-        return False
+def is_safe(numbers):
+  increasing = numbers[0] < numbers[1]
+  for i in range(len(numbers) - 1):
+    difference = numbers[i] - numbers[i + 1]
+    if (increasing and difference > 0) or (not increasing and difference < 0):
+      return False
+    if abs(difference) < 1 or abs(difference) > 3:
+      return False
   return True
 
 
 for line in lines:
   numbers = line.split(' ') 
   a = [int(num) for num in numbers]
-  safe = isSafe(a)
+  safe = is_safe(a)
   if safe:
     result += 1
     continue
@@ -31,7 +28,7 @@ for line in lines:
   for i in range(len(a)):
     b = a.copy()
     del b[i] 
-    if isSafe(b):
+    if is_safe(b):
       result +=1 
       break 
     
