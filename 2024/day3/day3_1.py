a3ad1ee91a4d96fb77ffe5bfc3b437117cc26fd2
Roberto De la Parra <roberto.delaparra@ottonova.de>
2024-12-03 08:35:14 +0100

    part one

---
+++ 2024/day3/day3_1.py
@@ -0,0 +1,28 @@
+
+file = open('2024/day3/input.txt', 'r')
+lines = file.readlines()
+
+result = 0
+a = []
+
+import re
+
+def extract_and_multiply(data):
+    pattern = r'mul\(\d{1,3},\d{1,3}\)'
+    
+    matches = re.findall(pattern, data)
+    
+    results = 0
+    for match in matches:
+        numbers = re.findall(r'\d+', match)
+        x, y = map(int, numbers)
+        
+        results += x * y
+    
+    return results
+
+for line in lines:
+    result += extract_and_multiply(line)
+    
+  
+print('Solution 1.', result)
