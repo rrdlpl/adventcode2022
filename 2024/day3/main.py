7c209c28cf364f399978cafac2bef1758603f08e
Roberto De la Parra <roberto.delaparra@ottonova.de>
2024-12-05 13:42:04 +0100

    day five

--- 2024/day3/main.py
+++ 2024/day3/main.py
@@ -1,11 +1,23 @@
 
 import re
-
 file = open('2024/day3/input.txt', 'r')
 lines = file.readlines()
 
+def part_one(data):
+    pattern = r'mul\(\d{1,3},\d{1,3}\)'
+    
+    matches = re.findall(pattern, data)
+    
+    results = 0
+    for match in matches:
+        numbers = re.findall(r'\d+', match)
+        x, y = map(int, numbers)
+        
+        results += x * y
+    
+    return results
 
-def extract_and_multiply(data):
+def part_two(data):
     result = 0
     enabled_multiplication = True
     for match in re.findall(r'don\'t\(\)|do\(\)|mul\(\d+,\d+\)', data):
@@ -22,8 +34,7 @@ def extract_and_multiply(data):
 
     return result
 
-
-result = extract_and_multiply(''.join(lines))
     
-  
-print('Solution 2.', result)
+print('Solution 1.', part_one(''.join(lines)))
+
+print('Solution 2.', part_two(''.join(lines)))
