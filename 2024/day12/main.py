503fc6d3a7ded64af59da638fec8a6eee21ecaae
Roberto De la Parra <roberto.delaparra@ottonova.de>
2024-12-12 13:20:29 +0100

    day 12

--- 2024/day12/main.py
+++ 2024/day12/main.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 import time
 file = open('2024/day12/input.txt', 'r')
 lines = file.readlines()
@@ -15,7 +16,6 @@ def part_one():
             neighbor = (x + dx, y + dy)
             if neighbor not in region:
                 perimeter += 1
-    
     return perimeter
 
   def dfs(i, j, plant, region):
@@ -32,12 +32,12 @@ def part_one():
   
   regions = []
   
-  for i in range(m):
-    for j in range(n):
-      if (i, j) not in visited:
+  for x in range(m):
+    for y in range(n):
+      if (x, y) not in visited:
         region = set()
-        dfs(i, j, grid[i][j], region)
-        regions.append((grid[i][j], region))
+        dfs(x, y, grid[x][y], region)
+        regions.append((grid[x][y], region))
 
   
   print('Regions: ')
@@ -47,9 +47,70 @@ def part_one():
   for plant, region in regions:
     perimeters += calculate_perimeter(region) * len(region)
   return perimeters 
+
+def part_two():
+  def count_sides(region):
+    top, right, bottom, left = set(), set(), set(), set()
+
+    for x, y in region:
+        if (x - 1, y) not in region: top.add((x, y))
+        if (x, y + 1) not in region: right.add((x, y))
+        if (x + 1, y) not in region: bottom.add((x, y))
+        if (x, y - 1) not in region: left.add((x, y))
+
+    sides = 0
+    for (x, y) in top:
+        if (x, y) in right:
+            sides += 1
+        elif (x - 1, y + 1) in left:
+            sides += 1
+        if (x, y) in left:
+            sides += 1
+        elif (x - 1, y - 1) in right:
+            sides += 1
+
+    for (x, y) in bottom:
+        if (x, y) in left:
+            sides += 1
+        elif (x + 1, y - 1) in right:
+            sides += 1
+        if (x, y) in right:
+            sides += 1
+        elif (x + 1, y + 1) in left:
+            sides += 1
+
+    return sides
+
+  def dfs(i, j, plant, region):
+    visited.add((i, j))
+    region.add((i, j))
+
+    for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+      new_x, new_y = i + dx, j + dy
+      if (new_x, new_y) in visited:
+        continue
+      if 0 <= new_x < m and 0 <= new_y < n and grid[new_x][new_y] == plant:
+        dfs(new_x, new_y, plant,region)
+
+  visited = set()
+  regions = []  
+  for x in range(m):
+    for y in range(n):
+      if (x, y) not in visited:
+        region = set()
+        dfs(x, y, grid[x][y], region)
+        regions.append((grid[x][y], region))
+
+  
+  print('Regions: ')
+  print(regions)
+  
+  sides = 0
+  for plant, region in regions:
+    sides += count_sides(region) * len(region)
+  return sides 
   
  
-# 62
-# 9387
+# print('Solution 1.', part_one())
 
-print('Solution 1.', part_one())
+print('Solution 2.', part_two())
