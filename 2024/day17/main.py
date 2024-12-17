cacea052ca82afaa136dbf52e6fd8d72fda469f6
Roberto De la Parra <roberto.delaparra@ottonova.de>
2024-12-17 10:18:01 +0100

    buggy version one

---
+++ 2024/day17/main.py
@@ -0,0 +1,147 @@
+# a = 25358015
+# b= 0
+# c = 0
+
+# program = ""
+
+class Computer:
+  def __init__(self, a, b, c):
+    self.a = a
+    self.b = b
+    self.c = c
+    self.outs = []
+    self.pointer = 0
+  
+  def run(self, program):
+    while self.pointer < len(program):
+      opcode, operand = program[self.pointer], program[self.pointer + 1]
+      if opcode == 0:
+        self.adv(operand)
+      elif opcode == 1:
+        self.bxl(operand)
+      elif opcode == 2:
+        self.bst(operand)
+      elif opcode == 3:
+        self.jnz(operand)
+      elif opcode == 4:
+        self.bxc(operand)
+      elif opcode == 5:
+        self.out(operand)
+      elif opcode == 6:
+        self.bdv(operand)
+      elif opcode == 7:
+        self.cdv(operand)
+   
+   
+  def adv(self, operand): #0
+    n = self.a // (2 ** self.get_combo_operand_value(operand))
+    self.a = n
+    self.pointer += 2
+
+  def bxl(self, operand): #1
+    self.b = self.b ^ operand
+    self.pointer += 2
+  
+  def bst(self, operand): #2
+    t = self.get_combo_operand_value(operand) % 8
+    self.b = t
+    self.pointer += 2
+        
+  def jnz(self, operand): #3
+    if self.a == 0:
+      self.pointer += 2
+      return
+    self.pointer = operand
+  
+  def bxc(self, _operand): #4
+    self.b = self.b ^ self.c
+    self.pointer += 2
+  
+  def out(self, operand): #5
+    value = self.get_combo_operand_value(operand) % 8
+    self.outs.append(value)
+    self.pointer += 2
+    # print('out', value)
+  
+  def bdv(self, operand): #6
+    self.b = self.a // (2 ** self.get_combo_operand_value(operand))
+    self.pointer += 2
+  
+  def cdv(self, operand): #7
+    self.c = self.a // (2 ** self.get_combo_operand_value(operand))
+    self.pointer += 2
+  
+  def get_combo_operand_value(self, operand):
+    if 0 <= operand <= 3:
+        return operand
+    if operand == 4:
+        return self.a
+    if operand == 5:
+        return self.b
+    if operand == 6:
+        return self.c
+    return -1
+
+  
+  def print(self):
+    print(','.join([str(x) for x in self.outs]))
+
+a =  729
+b = 0
+c = 0
+
+program = [int(instruction) for instruction in "0,1,5,4,3,0".split(",")]
+
+
+
+
+# Here are some examples of instruction operation:
+
+# If register C contains 9, the program 2,6 would set register B to 1.
+computer = Computer(0, 0, 9)
+computer.run([2, 6])
+print("If register C contains 9, the program 2,6 would set register B to 1.", computer.b == 1)
+
+# If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.
+computer = Computer(10, 0, 0)
+
+print("If register A contains 10, the program 5,0,5,1,5,4 would output 0,1,2.")
+computer.run([5, 0, 5, 1, 5, 4])
+computer.print()
+print('')
+
+# If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A.
+print("If register A contains 2024, the program 0,1,5,4,3,0 would output 4,2,5,6,7,7,7,7,3,1,0 and leave 0 in register A. Register =", computer.a, computer.a == 0)
+computer = Computer(2024, 0, 0)
+computer.run([0, 1, 5, 4, 3, 0 ])
+computer.print()
+print('')
+
+# # If register B contains 29, the program 1,7 would set register B to 26.
+print("If register B contains 29, the program 1,7 would set register B to 26. B =", computer.b, computer.b == 26)
+computer = Computer(0, 29, 0)
+computer.run([1, 7])
+print('')
+
+# # If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.
+computer = Computer(0, 2024, 43690)
+computer.run([4, 0])
+print("If register B contains 2024 and register C contains 43690, the program 4,0 would set register B to 44354.", computer.b == 44354)
+print('')
+
+
+print("\n\n Test example: Expected output: 4,6,3,5,6,3,5,2,1,0")
+computer = Computer(729, 0, 0)
+computer.run([0,1,5,4,3,0])
+computer.print()
+print('')
+
+
+part_one = Computer(25358015, 0, 0)
+part_one.run([2,4,1,1,7,5,0,3,4,7,1,6,5,5,3,0])
+print("\n\n Registers part 1:", part_one.a, part_one.b, part_one.c)
+
+print('Solution 1:')
+part_one.print()
+
+#attempt 1: 2,7,2,5,1,2,7,3,7
